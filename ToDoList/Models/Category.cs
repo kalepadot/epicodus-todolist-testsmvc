18da6ceea28e5488657117e272801e29008e3e4a
Geoff Goetz and jeremy kale padot <geoff@>
2020-03-12 09:37:21 -0700

    Add third and fourth test, passing.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -9,12 +9,22 @@ namespace ToDoList.Models
     public int Id { get; }
     public List<Item> Items { get; set; }
 
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
     public Category(string categoryName)
     {
       Name = categoryName;
       _instances.Add(this);
       Id = _instances.Count;
       Items = new List<Item>{};
+    }
+    
+    public static List<Category> GetAll()
+    {
+      return _instances;
     }
   }
 }
